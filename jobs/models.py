f23a0f52bb80bf8d8ac048f5c15a12b0d6ba5453
burakatak <atak_burak_75@hotmail.com>
2024-04-01 15:05:46 +0300

    Update models

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ from .enums import WorkType, JobStatus
 
 
 class Job(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
     linkedin_job_id = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
     company = models.ForeignKey('company.Company', on_delete=models.CASCADE, null=True, blank=True)
