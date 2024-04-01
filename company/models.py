137eeb425d0699c98c392f9765b1804197f33fc8
burakatak <atak_burak_75@hotmail.com>
2024-04-02 01:54:44 +0300

    Update jobs.service

--- company/models.py
+++ company/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=300)
-    logo = models.URLField(null=True, blank=True)
+    name = models.CharField(max_length=300, default="")
+    logo = models.URLField(null=True, blank=True, max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
